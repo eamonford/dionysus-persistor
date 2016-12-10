8d57937725f95f01d7774b3f1e5ffe5dd0abec57
Eamon Ford <eamonford@gmail.com>
2016-12-10 15:20:47 -0600

    persist all readings in influx

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import datetime
 from influxdb import InfluxDBClient
 
+influxClient = InfluxDBClient('db', 8086, 'root', 'root', 'example')
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to mosquitto with result code "+str(rc))
@@ -14,15 +15,15 @@ def on_message(client, userdata, msg):
                    "measurement": "moisture",
                    "time": datetime.datetime.now().isoformat(),
                    "fields": {
-                       "value": messageDict["value"]
+                       "value": messageDict["value"],
+                       "device_id": messageDict["device_id"]
                    }
                }
            ]
         print json_body
-        
+        influxClient.write_points(points=json_body, database="dionysus_readings")
 
 def main():
-    influxClient = InfluxDBClient('db', 8086, 'root', 'root', 'example')
     mqttClient = mqtt.Client()
     mqttClient.on_connect = on_connect
     mqttClient.on_message = on_message
